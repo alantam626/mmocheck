f6ec4ba2b492b9ed8a589b973fd008f286d4d5c0
Alan Tam <tam.alan7@gmail.com>
2022-02-10 13:27:50 -0800

    deploy

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import AuthPage from '../AuthPage/AuthPage';
 import NavBar from '../../components/NavBar/NavBar';
 import GameIndexPage from '../GameIndexPage/GameIndexPage'
@@ -8,6 +8,7 @@ import { getUser } from '../../utilities/users-service';
 import GameDetailPage from '../GameDetailPage/GameDetailPage';
 import UserPage from '../UserPage/UserPage';
 import FavoritesPage from '../FavoritesPage/FavoritesPage';
+
 
 export default function App() {
   const [user, setUser] = useState(getUser());
@@ -23,6 +24,7 @@ export default function App() {
             <Route path="/index/:id" element={<GameDetailPage />} />
             <Route path="/favorites" element={<FavoritesPage />} />
             <Route path="/userpage" element={<UserPage />} />
+            <Route path="/*" element={<Navigate to="/index"/>} /> 
           </Routes>
         </>
           :
